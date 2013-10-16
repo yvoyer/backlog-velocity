313b995d5b3c37d9ed75aa2e6ffca0c48da705f3
Yannick Voyer <yan.voyer@gmail.com>
2013-10-15 20:52:17 -0400

    Code cleanup

--- src/Star/Component/Sprint/Repository/Doctrine/DoctrineObjectManagerAdapter.php
+++ src/Star/Component/Sprint/Repository/Doctrine/DoctrineObjectManagerAdapter.php
@@ -13,9 +13,7 @@ use Star\Component\Sprint\Entity\Repository\SprintMemberRepository;
 use Star\Component\Sprint\Entity\Repository\SprintRepository;
 use Star\Component\Sprint\Entity\Repository\TeamMemberRepository;
 use Star\Component\Sprint\Entity\Repository\TeamRepository;
-use Star\Component\Sprint\Mapping\Entity;
 use Star\Component\Sprint\Repository\Mapping;
-use Star\Component\Sprint\Repository\Repository;
 use Star\Component\Sprint\Repository\RepositoryManager;
 
 /**
@@ -24,10 +22,8 @@ use Star\Component\Sprint\Repository\RepositoryManager;
  * @author  Yannick Voyer (http://github.com/yvoyer)
  *
  * @package Star\Component\Sprint\Repository\Doctrine
- *
- * @todo Create a RepositoryAdapter
  */
-class DoctrineObjectManagerAdapter implements RepositoryManager, Repository // @todo Remove Repository implement
+class DoctrineObjectManagerAdapter implements RepositoryManager
 {
     /**
      * @var ObjectManager
@@ -112,62 +108,5 @@ class DoctrineObjectManagerAdapter implements RepositoryManager, Repository // @
             $this->mapping->getTeamMemberMapping(),
             $this->objectManager
         );
-    }
-
-    /**
-     * Returns all the object from one repository.
-     *
-     * @return array
-     */
-    public function findAll()
-    {
-        throw new \Exception("Feature can't be used from this scope");
-        // TODO: Implement findAll() method.
-    }
-
-    /**
-     * Returns the object linked with the $id.
-     *
-     * @param mixed $id
-     *
-     * @return object
-     */
-    public function find($id)
-    {
-        throw new \Exception("Feature can't be used from this scope");
-        // TODO: Implement find() method.
-    }
-
-    /**
-     * Returns the object matching the $criteria.
-     *
-     * @param array $criteria
-     *
-     * @return object
-     */
-    public function findOneBy(array $criteria)
-    {
-        throw new \Exception("Feature can't be used from this scope");
-        // TODO: Implement findOneBy() method.
-    }
-
-    /**
-     * Add the $object linked to the $id.
-     *
-     * @param Entity $object
-     */
-    public function add(Entity $object)
-    {
-        $this->objectManager->persist($object);
-        // TODO: Implement add() method.
-    }
-
-    /**
-     * Save the $object in the repository.
-     */
-    public function save()
-    {
-        $this->objectManager->flush();
-        // TODO: Implement save() method.
     }
 }
