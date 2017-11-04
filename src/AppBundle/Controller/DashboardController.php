3bbe7ac0ec5881f16d746a08e16b9f2e251068c5
Yannick Voyer <yan.voyer@gmail.com>
2017-11-04 09:41:32 -0400

    Add symfony integration
    * Fixes #70
    * Add DashboardController

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/", name="dashboard")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
