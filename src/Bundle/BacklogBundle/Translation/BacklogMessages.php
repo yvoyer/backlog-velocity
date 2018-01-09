df2b2ddac60cdeee2e2375f2a6c2dba71512d1b8
Yannick Voyer <yan.voyer@gmail.com>
2018-01-09 13:59:04 +0000

    * Add twig filter to output time ago * Add twig function that returns the dates of a sprint using Queries

--- src/Bundle/BacklogBundle/Translation/BacklogMessages.php
+++ src/Bundle/BacklogBundle/Translation/BacklogMessages.php
@@ -2,7 +2,10 @@
 
 namespace Star\BacklogVelocity\Bundle\BacklogBundle\Translation;
 
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\Translation\Translator;
 use Symfony\Component\Translation\TranslatorInterface;
 
 final class BacklogMessages
@@ -57,5 +60,22 @@ final class BacklogMessages
      */
     public function addError(string $message, array $parameters = []) :void {
         $this->flashes->add('danger', $this->translator->trans($message, $parameters));
+    }
+
+    public function message(string $key, array $arguments = []) :string
+    {
+       // var_dump($this->translator);
+        return $this->translator->trans($key, $arguments, 'messages');
+    }
+
+    public static function fixture() :self
+    {
+        $translator = new Translator('en');
+        $loader = new YamlFileLoader();
+        $loader->load($resource = __DIR__ . '/../Resources/translations/messages.en.yml', 'en');
+        $translator->addLoader('yml', $loader);
+        $translator->addResource('yml', $resource, 'en');
+
+        return new self($translator, new FlashBag());
     }
 }
