4b38f56f9a9f70f0a79d5cca05d0d37ac1e803f3
Yannick Voyer <yan.voyer@gmail.com>
2020-09-29 20:33:25 -0400

    * update migrations

--- app/Migrations/Version20180110000000.php
+++ app/Migrations/Version20180110000000.php
@@ -2,15 +2,15 @@
 
 namespace Star\BacklogVelocity\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
 class Version20180110000000 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
@@ -65,7 +65,7 @@ class Version20180110000000 extends AbstractMigration
 
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
--- app/Migrations/Version20180111000000.php
+++ app/Migrations/Version20180111000000.php
@@ -2,15 +2,15 @@
 
 namespace Star\BacklogVelocity\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
 class Version20180111000000 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
@@ -36,7 +36,7 @@ class Version20180111000000 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_13602B65296CD8AE ON backlog_team_members (team_id)');
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
